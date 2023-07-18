e6d95d1b8a6fcf476f7d3b5f1139d88c73966e86
Vincent Santaiti <vsantaiti@gmail.com>
2023-07-18 16:16:31 -0400

    add mogrify

--- postgres.py
+++ postgres.py
@@ -13,3 +13,17 @@ result = db_cur.fetchall()
 for res in result:
 	print(res)
 db_conn.close()
+
+def test_mogrify():
+	db_conn = psycopg2.connect(dsn)
+	db_cur = db_conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+	ids = [1, 2, 3, 4]
+	id_str = ','.join(str(i) for i in ids)
+	sql = """select column from table where column in (%(id_str)s);"""
+	data = {'id_str': id_str}
+	query = dm_cur.mogrify(sql, data).decode("utf-8")
+	db_cur.execute(query)
+	result = db_cur.fetchall()
+	for res in result:
+		print(res)
+	db_conn.close()
